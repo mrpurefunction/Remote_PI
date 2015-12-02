fcbb70f3539e7fb065dfa135203d8801ddedc03e
mrpurefunction <mrpurefunction@sina.com>
2015-12-02 12:34:00 +0800

    update_20151202

--- Remote_Pihis/ws_test_20150310/LocalPIData/PIHisData.cs
+++ Remote_Pihis/ws_test_20150310/LocalPIData/PIHisData.cs
@@ -10,12 +10,17 @@ namespace LocalPIData
     /// </summary>
     public class PIHisData
     {
+        public static string ip = (string)(new System.Configuration.AppSettingsReader()).GetValue("ip", typeof(string));
+        public static string user = (string)(new System.Configuration.AppSettingsReader()).GetValue("username", typeof(string));
+        public static string psd = (string)(new System.Configuration.AppSettingsReader()).GetValue("password", typeof(string));
+
         /// <summary>
         /// constructor
         /// </summary>
         public PIHisData()
         {
-            new PI.PIFunc2("10.136.36.42", "piadmin", "");
+            //new PI.PIFunc2("10.136.36.42", "piadmin", "");
+            new PI.PIFunc2(ip, user, psd);
         }
 
         /// <summary>
@@ -26,7 +31,8 @@ namespace LocalPIData
         /// <returns></returns>
         public double? GetHisValue(string pn, DateTime ts)
         {
-            PI.RetVal rv = (new PI.PIFunc2("10.136.36.42", "piadmin", "")).GetPointHisValue(pn, ts);
+            //PI.RetVal rv = (new PI.PIFunc2("10.136.36.42", "piadmin", "")).GetPointHisValue(pn, ts);
+            PI.RetVal rv = (new PI.PIFunc2(ip, user, psd)).GetPointHisValue(pn, ts);
             return rv == null ? (double?)null : rv.pvalue;
         }
     }
